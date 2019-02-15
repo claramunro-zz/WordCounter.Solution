6ec61508474bf0daae741172c936d40adca14e8c
Clara Munro <clarajmunro@gmail.com>
2019-02-15 15:12:36 -0800

    added variables to Word.cs

---
+++ WordCounter/Models/Word.cs
@@ -0,0 +1,12 @@
+namespace WordCounter
+{
+  public class Word
+  {
+    public int wordCount;
+    private string inputWord;
+    private string inputSentence;
+
+      // logic will go here
+
+  }
+}
