61b690b0e6bb08d89ff232f130dd30d95a39d0c7
Clara Munro <clarajmunro@gmail.com>
2019-02-15 16:44:39 -0800

    added cs file to tests / modulestests

---
+++ WordCounter.Tests/ModelTests/WordCountTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCount;
+
+namespace WordCount.Tests
+{
+  [TestClass]
+  public class WordCountTest
+  {
+    [TestMethod]
+    public void WordCount_IsInSentence_True()
+    {
+      //testing code will go here
+    }
+  }
+}
