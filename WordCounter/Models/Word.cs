cac923be497ca2e7d38c312100472df7a6ddb7ed
Clara Munro <clarajmunro@gmail.com>
2019-02-19 08:48:25 -0800

    made private keys to public keys

--- WordCounter/Models/Word.cs
+++ WordCounter/Models/Word.cs
@@ -6,6 +6,14 @@ namespace WordCounter
     private string _inputSentence;
     private string _inputWord;
 
+    // makes private words public
+   public Word(string inputSentence, string inputWord, int wordCount)
+   {
+     _wordCount = wordCount;
+     _inputWord = inputWord;
+     _inputSentence = inputSentence;
+   }
+
     public int MatchCounter(string inputSentence, string inputWord, int wordCount)
     {
       string[] split = inputSentence.Split(' ');
